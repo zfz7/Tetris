d0390e3510809593785f3981b8f709a08c23a381
Daniel Eichman <D.Eichman@outlook.com>
2021-03-17 17:15:30 -0700

    create setupEC2.sh

---
+++ setupEC2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Require script to be run as root (or with sudo)
+function super-user-check() {
+  if [ "$EUID" -ne 0 ]; then
+    echo "You need to run this script as super user."
+    exit
+  fi
+}
+
+# Check for root
+super-user-check
+
+#Install Docker
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce -y
+
+#Install Docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#Install openJDK
+apt install default-jre -y
+apt install openjdk-11-jdk -y
+
+#Install Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt update
+apt install yarn -y
