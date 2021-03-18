02982c7e7aa86c759ea04a2b6339749e3b13b839
Daniel Eichman <D.Eichman@outlook.com>
2021-03-17 19:14:13 -0700

    perp for certs

--- setupEC2.sh
+++ setupEC2.sh
@@ -36,3 +36,8 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt update
 apt install yarn -y
+
+#Install certbot
+snap install core
+snap refresh core
+snap install --classic certbot
