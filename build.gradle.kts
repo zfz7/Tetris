1275f1eb351576ac33433101641052602a502139
Daniel Eichman <D.Eichman@outlook.com>
2021-06-02 11:34:31 -0500

    Update kotlin to 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	val kotlinVersion = "1.4.31"
+	val kotlinVersion = "1.5.10"
 	val springBootVersion = "2.4.3"
 	val springDependencyManagementVersion = "1.0.11.RELEASE"
 	val nodePluginVersion = "3.0.1"
